4d9f3cce9d9696b8aadf382095ad93cedc9b8b12
greewank <96386149+greewank@users.noreply.github.com>
2022-08-29 18:47:09 -0400

    Time: 119 ms (23.15%), Space: 43.1 MB (63.66%) - LeetHub

--- 67-add-binary/67-add-binary.js
+++ 67-add-binary/67-add-binary.js
@@ -8,18 +8,19 @@ var addBinary = function(a, b) {
     let carry = 0;
     let i = a.length - 1;
     let j = b.length - 1;
-    while (i >= 0 || j >= 0){
-        let A = a[i] ? a[i] : '0';
-        let B = b[j] ? b[j] : '0';
-        sum = String(A ^ B ^ carry) + sum;
-        if (A === B && A !== String(carry)){
+    while(i>=0 || j>=0){
+        let A = a[i] ? a[i]: '0';
+        let B = b[j] ? b[j]: '0';
+        sum = String (A^B^carry)+sum;
+        if (A===B && A!=String(carry)){
+//             So, !carry is either false or true and Number will convert it to 0 or 1.
             carry = Number(!carry);
         }
         i--;
         j--;
     }
-    if(carry){
-        sum = String(carry) + sum;
-    }
+        if(carry){
+            sum = String(carry) + sum;
+        }
     return sum;
 };
