60eade7f0ef76ceb9c440da14b47a23b16ed8391
greewank <96386149+greewank@users.noreply.github.com>
2022-08-31 20:33:18 -0400

    Time: 81 ms (75.70%), Space: 42.6 MB (50.32%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.js
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -4,8 +4,8 @@
  */
 var longestCommonPrefix = function(s) {
     s.sort();
-    for(let i=0; i<s[0].length;i++){
-        if(s[0][i]!==s[s.length-1][i]) return s[0].slice(0,i);
+    for(let i = 0; i<=s[0].length; i++){
+        if(s[0][i] !== s[s.length-1][i]) return s[0].slice(0, i);
     }
     return s[0];
 }
