bd8fbfdee051c78dba7a17933fb48fa079d830a4
greewank <96386149+greewank@users.noreply.github.com>
2022-09-06 20:27:06 -0400

    Time: 63 ms (99.69%), Space: 44.4 MB (63.97%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.js
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.js
@@ -10,14 +10,18 @@
  * @param {number[]} nums
  * @return {TreeNode}
  */
-var sortedArrayToBST = function(nums, left = 0, right = nums.length - 1) {
-    if (left > right) return null;
-    let mid = Math.floor((left+right) / 2);
+var sortedArrayToBST = function(nums, left=0, right = nums.length-1) {
+//     Initialize the pointers left, mid and right. Left is first, mid is mid and 
+//     right is last element
+    if (left > right) return null; //because tree won't be balanced
+    
+    let mid = Math.floor((left + right ) / 2);
     let root = new TreeNode(nums[mid]);
     
     root.left = sortedArrayToBST(nums, left, mid-1);
     root.right = sortedArrayToBST(nums, mid+1, right);
     
     return root;
+    
     
 };
