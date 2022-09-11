08c943a3a50d1a2583734d492433ccb5c01e512c
greewank <96386149+greewank@users.noreply.github.com>
2022-09-10 21:21:12 -0400

    Time: 163 ms (39.20%), Space: 50.4 MB (30.61%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.js
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.js
@@ -12,13 +12,12 @@
  * @return {TreeNode}
  */
 var mergeTrees = function(root1, root2) {
-    if(root1 === null){
+    if(root1 === null)
         return root2;
-    }
-    if(root2 === null){
+    if(root2 === null)
         return root1;
-    }
     root1.val += root2.val;
+    
     root1.left = mergeTrees(root1.left, root2.left);
     root1.right = mergeTrees(root1.right, root2.right);
     
