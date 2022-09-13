bc36f8b8230ab384cc3187cbc12b666c1bd293b2
greewank <96386149+greewank@users.noreply.github.com>
2022-09-12 20:36:01 -0400

    Time: 159 ms (36.68%), Space: 50.6 MB (73.57%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,11 +3,20 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let naan = nums.sort((a,b)=> a - b);
-    for(let i = 0; i<naan.length; i++){
-        if(naan[i] === naan[i+1]){
-            return true;
-        }
+//     First method (using sort)
+    // let naan = nums.sort((a,b)=> a - b);
+    // for(let i = 0; i<naan.length; i++){
+    //     if(naan[i] === naan[i+1]){
+    //         return true;
+    //     }
+    // }
+    // return false;
+    
+//   Second method (Using Set):
+    const set = [...new Set(nums)];
+    if (set.length !== nums.length){
+        return true;
     }
     return false;
+    
 };
