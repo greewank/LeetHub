216f28370907e7af522d7099e7c83c2e487ed404
greewank <96386149+greewank@users.noreply.github.com>
2022-09-24 21:25:07 -0400

    Time: 136 ms (33.60%), Space: 45.6 MB (51.79%) - LeetHub

--- 136-single-number/136-single-number.js
+++ 136-single-number/136-single-number.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    nums = nums.sort((a,b) => a-b);
-    for(let i=0; i<nums.length; i++){
-        if(nums[i] == nums[i-1] || nums[i] == nums[i+1]){
+    nums = nums.sort((a,b)=> a-b);
+    for(let i = 0; i<nums.length; i++){
+        if(nums[i] === nums[i-1] || nums[i] === nums[i+1]){
             continue;
-        } else{
+        }else{
             return nums[i];
         }
     }
