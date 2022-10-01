b4f6f2ca83bed2f8031f385d0cecaa858401b0b9
greewank <96386149+greewank@users.noreply.github.com>
2022-09-30 22:17:30 -0400

    Time: 111 ms (73.97%), Space: 42.7 MB (90.78%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,18 +4,15 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if (s.length !== t.length){
-        return false;
+   if(s.length !== t.length){
+       return false;
     } 
-    //     When creating map, you can create empty one and set key-values accordingly.
     let sMap = {};
     let tMap = {};
     for(let i = 0; i<s.length; i++){
         sMap[s[i]] = sMap[s[i]] + 1 || 1;
-        console.log(sMap[s[i]]);
         tMap[t[i]] = tMap[t[i]] + 1 || 1;
     }
-    
     for(let k in sMap){
         if(sMap[k] !== tMap[k]){
             return false;
