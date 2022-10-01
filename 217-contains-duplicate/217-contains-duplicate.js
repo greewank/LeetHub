2b91f0668ac8f08bb8846c561e14fd6b96844f59
greewank <96386149+greewank@users.noreply.github.com>
2022-09-30 22:04:45 -0400

    Time: 85 ms (96.78%), Space: 49.3 MB (90.45%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,18 +3,22 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let hello = nums.sort((a,b)=>{
-        return a-b;
-    });
-    for(let i = 0; i<hello.length; i++){
-        if(hello[i] === hello[i+1]){
+    // let hello = nums.sort((a,b)=>{
+    //     return a-b;
+    // });
+    // for(let i = 0; i<hello.length; i++){
+    //     if(hello[i] === hello[i+1]){
+    //         return true;
+    //     }
+    // }
+    // return false;
+    
+    let hash = new Set();
+    for(let i = 0; i<nums.length; i++){
+        if(hash.has(nums[i])){
             return true;
         }
+        hash.add(nums[i]);
     }
     return false;
-    
-    // let hash = {};
-    // for(let i = 0; i<nums.length; i++){
-    //     hash[nums[i]]
-    // }
 };
