25378e235f16df88683a5eda07fdb5b86e1cb7e7
greewank <96386149+greewank@users.noreply.github.com>
2022-10-04 20:00:14 -0400

    Time: 239 ms (33.75%), Space: 52.6 MB (82.72%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -3,17 +3,14 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-  let sortedGuy = strs.map(str => str.split("").sort().join());
-  let hash ={};
-//     So we sort the original array and create a hash. In the hash we compare the sorted value as 
-//     key with the original array as value and push new ones.
-  for(let i =0; i<strs.length; i++){
-      if(!hash[sortedGuy[i]]){
-          hash[sortedGuy[i]] = [strs[i]];
-      }else{
-          hash[sortedGuy[i]].push(strs[i]);
-      }
-  }
-//     Object.values returns an ARRAY of object's values (so from that key value pair)
+    let thanksGod = strs.map(str => str.split("").sort().join());
+    let hash = {};
+    for(let i =0; i<strs.length; i++){
+        if(!hash[thanksGod[i]]){
+            hash[thanksGod[i]] = [strs[i]];
+        }else{
+            hash[thanksGod[i]].push(strs[i]);
+        }
+    }
     return Object.values(hash);
 };
