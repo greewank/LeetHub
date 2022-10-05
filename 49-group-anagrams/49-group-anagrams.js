d263459029e5fb6e801e53c752782ad461f6d972
greewank <96386149+greewank@users.noreply.github.com>
2022-10-04 20:00:55 -0400

    Time: 127 ms (94.26%), Space: 52.7 MB (76.85%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -5,6 +5,9 @@
 var groupAnagrams = function(strs) {
     let thanksGod = strs.map(str => str.split("").sort().join());
     let hash = {};
+    //     So we sort the original array and create a hash. 
+    // In the hash we compare the sorted value as 
+    //     key with the original array as value and push new ones.
     for(let i =0; i<strs.length; i++){
         if(!hash[thanksGod[i]]){
             hash[thanksGod[i]] = [strs[i]];
@@ -12,5 +15,6 @@ var groupAnagrams = function(strs) {
             hash[thanksGod[i]].push(strs[i]);
         }
     }
+    //     Object.values returns an ARRAY of object's values (so from that key value pair)
     return Object.values(hash);
 };
