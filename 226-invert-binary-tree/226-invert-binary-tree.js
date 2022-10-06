5738d7496ce3d165df375ddd60b2ddc99ad6f6b4
greewank <96386149+greewank@users.noreply.github.com>
2022-10-06 19:35:32 -0400

    Time: 99 ms (39.47%), Space: 42.2 MB (69.03%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.js
+++ 226-invert-binary-tree/226-invert-binary-tree.js
@@ -14,8 +14,8 @@ var invertTree = function(root) {
     if(!root){
         return root;
     }
-    let temp = root.left; 
-    root.left = root.right;
+    let temp = root.left;
+    root.left= root.right;
     root.right = temp;
     invertTree(root.left);
     invertTree(root.right);
