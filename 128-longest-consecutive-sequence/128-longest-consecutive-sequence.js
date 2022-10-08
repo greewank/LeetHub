29c6562ff387bd171379448cfc838bf344847430
greewank <96386149+greewank@users.noreply.github.com>
2022-10-07 23:08:15 -0400

    Time: 115 ms (94.29%), Space: 57.6 MB (63.78%) - LeetHub

---
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+        let set = new Set();
+    for(const n of nums) {
+        set.add(n);
+    }
+    let max = 0;
+    for(let n of set) {
+        if(!set.has(n-1)) {
+            let count = 0;
+            while(set.has(n++)) {
+                count++;
+            }
+            max = Math.max(count, max);
+        }
+    }
+    return max;
+
+};
