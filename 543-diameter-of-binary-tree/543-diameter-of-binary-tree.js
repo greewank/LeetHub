ade4b1cb383f3a685b3f9b3619653dd5ab022ba0
greewank <96386149+greewank@users.noreply.github.com>
2022-10-08 17:48:16 -0400

    Time: 127 ms (29.89%), Space: 45.9 MB (33.73%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.js
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.js
@@ -11,15 +11,15 @@
  * @return {number}
  */
 var diameterOfBinaryTree = function(root) {
-   let result = 0;
+    let result =0;
     function dfs(root){
         if(!root){
             return -1;
-        }
+        }   
         let left = dfs(root.left);
         let right = dfs(root.right);
-        result = Math.max(result, 2 + left + right);
-        return 1 + Math.max(left,right);
+        result = Math.max(result, left + right + 2);
+        return 1+Math.max(left, right);
     }
     dfs(root);
     return result;
