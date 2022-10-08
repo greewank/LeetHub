9155dd5a5b97486fb6072de43bc5d775ac826af5
greewank <96386149+greewank@users.noreply.github.com>
2022-10-08 19:13:17 -0400

    Time: 294 ms (57.99%), Space: 49.4 MB (99.31%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -3,6 +3,25 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    let hello = x.toString().split('').reverse().join('');
-    return (hello === x.toString());
-};
+//     Using Strings (Method 1)
+    // let y = x.toString();
+    // for(let i = 0; i<(y.length)+1; i++){
+    //     if(y.charAt(i) != y.charAt(y.length-(i+1))){
+    //         return false;
+    //     }
+    // }
+    // return true;
+    
+//     Without strings (Method 2)
+    if(x<0){
+        return false;
+    }
+    let revNum = 0;
+    let temp = x;
+    while(temp > 0){
+        let digit = temp %10;
+        revNum = revNum * 10 + digit;
+        temp = Math.floor(temp/10);
+    }
+    return (revNum === x)
+}
