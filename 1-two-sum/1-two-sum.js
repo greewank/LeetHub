ca0a24e4af8c513e525ef53b205393d91681b12f
greewank <96386149+greewank@users.noreply.github.com>
2022-10-09 16:57:50 -0400

    Time: 93 ms (83.89%), Space: 45.2 MB (7.43%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -5,12 +5,12 @@
  */
 var twoSum = function(nums, target) {
     let map = {};
-    for(let i=0; i<nums.length; i++){
+    for(let i = 0; i<nums.length; i++){
         map[nums[i]] = i;
     }
-    for(let j = 0; j<nums.length; j++){
+    for(let j = 0 ; j<nums.length; j++){
         let result = target - nums[j];
-        if(map[result] && map[result]!==j){
+        if(map[result] && j!==map[result]){
             return [j, map[result]];
         }
     }
