57b2a0f1ae584d1d0c63dd86b673e77d77c8fd01
greewank <96386149+greewank@users.noreply.github.com>
2022-10-12 22:31:25 -0400

    Time: 241 ms (52.17%), Space: 52.7 MB (54.27%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,22 +3,28 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    nums = nums.sort((a,b)=> a-b);
-    let result = [];
+    nums = nums.sort((a,b)=>a-b);
+    let arr1=[];
     for(let i = 0; i<nums.length; i++){
-        let first = i+1, last = nums.length - 1, sum=0;
+        let first = i + 1;
+        let last = nums.length-1;
+        let sum = 0;
         while(first < last){
-        let sum = nums[i] + nums[first] + nums[last];
-        if(sum === 0){
-            result.push([nums[i], nums[first], nums[last]]);
-            while(nums[first]===nums[first+1]) first++;
-            while(nums[last]===nums[last-1]) last--;
-            first++;
-            last--;
-         } else if(sum <0) first++;
-         else last--;
+            sum = nums[i] + nums[first] + nums[last];
+            if(sum === 0){
+                arr1.push([nums[i], nums[first], nums[last]]);
+                while (nums[first] === nums[first+1]) first++;
+                while (nums[last] === nums[last+1]) last++; 
+                first++;
+                last++;
+            }
+            if(sum< 0){
+                first++;
+            } else{
+                last--;
+            }
         }
-        while(nums[i]===nums[i+1]) i++;
+        while(nums[i] === nums[i+1]) i++;
     }
-    return result;   
+    return arr1;
 };
