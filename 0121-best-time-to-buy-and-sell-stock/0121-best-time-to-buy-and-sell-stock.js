82475086a8aafb965fa65d404c99b5ca3716c6e7
greewank <96386149+greewank@users.noreply.github.com>
2022-10-13 19:54:20 -0400

    Time: 145 ms (42.61%), Space: 51.9 MB (23.12%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -5,14 +5,14 @@
 var maxProfit = function(prices) {
     let first = 0;
     let second = 1;
-    let max_Prof = 0;
+    let maxResult = 0;
     while(second < prices.length){
         if(prices[first]<prices[second]){
-            max_Prof = Math.max(prices[second]-prices[first], max_Prof);
-        }else{
+            maxResult = Math.max(prices[second]-prices[first], maxResult);
+        } else{
             first = second;
         }
         second++;
     }
-    return max_Prof;
+    return maxResult;
 };
