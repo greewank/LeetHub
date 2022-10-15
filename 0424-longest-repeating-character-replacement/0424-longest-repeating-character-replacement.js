d77c00a18e977cce2d437e9fea400822785e5c31
greewank <96386149+greewank@users.noreply.github.com>
2022-10-15 11:52:40 -0400

    Time: 124 ms (70.32%), Space: 42.1 MB (94.86%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -4,17 +4,16 @@
  * @return {number}
  */
 var characterReplacement = function(s, k) {
-//     This follows neetcodes solution in javascript
-    const charCunt = {};
-    let left=0, maxF=0, res=0;
+    let hashMap = {};
+    let left = 0, maxF = 0, res = 0;
     for(let right = 0; right<s.length; right++){
-        charCunt[s[right]] = charCunt[s[right]] + 1 || 1;
-        maxF=Math.max(maxF, charCunt[s[right]]);
-    while((right-left+1)-maxF > k){
-        charCunt[s[left]]--;
+        hashMap[s[right]] = hashMap[s[right]] + 1 || 1;
+        maxF = Math.max(maxF, hashMap[s[right]]);
+    while(((right-left)+1)-maxF > k){
+        hashMap[s[left]]--;
         left++;
     }
-    res = Math.max(res, (right-left+1));
+    res = Math.max(right-left+1, res);    
     }
     return res;
 };
