9c65dc064f7eac1f7aa29e9336d712dcc9bbeeb9
greewank <96386149+greewank@users.noreply.github.com>
2022-10-15 20:54:41 -0400

    Time: 68 ms (89.89%), Space: 42.3 MB (34.07%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+//     This is actually not a binary search algorithm solution (should use that but i couldn't)
+//     time complexity : O(m+n)
+    if(!matrix.length || !matrix[0].length) return false;
+    
+    let row = 0;
+    let column = matrix[0].length - 1;
+    while(column >=0 && row <= matrix.length -1){
+        if(matrix[row][column] === target) return true;
+        else if(matrix[row][column] > target) column--;
+        else if(matrix[row][column] < target) row++;
+    }
+    return false;
+};
