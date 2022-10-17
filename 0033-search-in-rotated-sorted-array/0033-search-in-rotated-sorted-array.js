fa9d7cb30e8fb0bdc80b8733a063d4da84c45c22
greewank <96386149+greewank@users.noreply.github.com>
2022-10-16 22:00:04 -0400

    Time: 84 ms (73.62%), Space: 42 MB (66.25%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -7,7 +7,7 @@ var search = function(nums, target) {
     let left = 0;
     let right = nums.length - 1;
     while(left <= right){
-        let mid = Math.floor(left + (right-left)/2);
+        let mid = Math.floor((left + right)/2);
         if(nums[mid]===target) return mid;
         
         if(nums[left]<=nums[mid]){
