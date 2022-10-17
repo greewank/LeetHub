55ee8d4126dc15b2f021ac873cfdc2ac35ce71dd
greewank <96386149+greewank@users.noreply.github.com>
2022-10-17 19:48:02 -0400

    Time: 95 ms (50.77%), Space: 42.1 MB (58.03%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -3,15 +3,15 @@
  * @return {number}
  */
 var findMin = function(nums) {
-   let left = 0;
-   let right = nums.length - 1;
-   while(left < right){
-       let mid = Math.floor((left + right)/2);
-       if(nums[mid] >= nums[right]){
-           left = mid + 1;
-       } else{
-           right = mid;
-       }
-   }
-   return nums[left];
+    let left = 0; 
+    let right = nums.length - 1;
+    while(left < right){
+        let mid = Math.floor((left+right)/2);
+        if(nums[mid] > nums[right]){
+            left = mid+1;
+        } else{
+            right = mid;
+        }
+    }
+    return nums[left];
 };
