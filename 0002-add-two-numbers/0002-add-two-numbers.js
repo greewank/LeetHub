33acbe94da6f5d1a7d97089b11eba2fc77666b66
greewank <96386149+greewank@users.noreply.github.com>
2022-10-21 19:30:49 -0400

    Time: 169 ms (61.94%), Space: 47.2 MB (63.80%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -11,12 +11,12 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    let List = new ListNode(0);
+    let newList = new ListNode(0);
     let sum = 0;
     let carry = 0;
-    let head = List;
-    while((l1!== null) ||(l2!== null) || (sum > 0)){
-        if(l1!== null){
+    let head = newList;
+    while((l1!==null) || (l2!==null) || (sum > 0)){
+        if(l1 !== null){
             sum = sum + l1.val;
             l1 = l1.next;
         }
@@ -25,14 +25,13 @@ var addTwoNumbers = function(l1, l2) {
             l2 = l2.next;
         }
         if(sum >= 10){
-            carry = 1;
             sum = sum - 10;
+            carry += 1;
         }
         head.next = new ListNode(sum);
         head = head.next;
-        
         sum = carry;
         carry = 0;
     }
-    return List.next;
+    return newList.next;
 };
