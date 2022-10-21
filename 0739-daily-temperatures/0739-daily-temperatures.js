25a9abcadc3a1271fed26f7c63af6d66906e87d8
greewank <96386149+greewank@users.noreply.github.com>
2022-10-21 19:03:51 -0400

    Time: 568 ms (36.50%), Space: 65.9 MB (35.21%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -3,17 +3,17 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(T) {
-//     Didn't understand the algorithm MAN (Ratta maar)
+//     Understood the algorithm now.
     const result = Array(T.length).fill(0);
-    const stack = [];
+    let stack = [];
     for(let i = 0; i<T.length; i++){
-        let top = stack[stack.length-1];
-        while(stack.length && T[top]<T[i]){
-            const naruto = stack.pop();
-            result[naruto] = i - naruto;
+      let top = stack[stack.length - 1];
+      while(stack.length && (T[top] < T[i])){
+            let hello = stack.pop();
+            result[hello] = i - hello;
             top = stack[stack.length - 1];
-        }
-        stack.push(i);
+      }
+      stack.push(i);
     }
     return result;
 };
