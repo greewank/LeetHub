7d30111317299f13258e4763b24cd91599ffe9e2
greewank <96386149+greewank@users.noreply.github.com>
2022-10-28 21:33:30 -0400

    Time: 116 ms (53.66%), Space: 44.7 MB (35.33%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum2 = function(candidates, target) {
+    let finalArr=[];
+    candidates = candidates.sort((a,b)=> a - b);
+    
+    const dfs = (i, candidates, target, slate)=>{
+        if(target < 0) return;
+        
+        if((target === 0)){
+            finalArr.push(slate.slice());
+            return;
+        }
+        
+        for(let j = i; j < candidates.length; j++){
+            if(i!==j && candidates[j]===candidates[j-1]) continue;
+            slate.push(candidates[j]);
+            dfs(j+1, candidates, target - candidates[j], slate);
+            slate.pop();
+        }
+    };
+    
+    dfs(0, candidates, target, []);
+    return finalArr;
+};
